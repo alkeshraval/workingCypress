16c9c8b210edc36f82e6b8ae4d43291d96e618b2
Chap Ambrose <chapambrose@gmail.com>
2018-09-13 12:03:48 -0500

    add a failing test

--- cypress/integration/simple_spec.js
+++ cypress/integration/simple_spec.js
@@ -8,3 +8,13 @@ describe('My First Test', function() {
       .contains('Hello World!')
   })
 })
+
+
+describe('My First Failing Test', function() {
+  it("Gets, types and asserts", function() {
+    cy.visit('0.0.0.0:'+port)
+
+    cy.get('h1')
+      .contains('Goodbye World!')
+  })
+})
